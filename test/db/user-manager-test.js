2b8d9445611572bd000a15588b55048295ef5946
fhersho <fhersho@gmail.com>
2016-08-30 17:31:16 -0500

    pa arriba

--- test/db/user-manager-test.js
+++ test/db/user-manager-test.js
@@ -14,3 +14,13 @@ test('getUserByMail', async t => {
   t.not(res, null, 'la resuesta no debe ser null')
   t.not(res.length, 0, 'la lista no debe ser vacia')
 })
+
+test('getAllByIdUser', async t => {
+  let res = await userManager.getUserById(1)
+  t.not(res, null, 'la resuesta no debe ser null')
+  t.is(res.length, 0, 'la lista debe ser vacia')
+
+  res = await userManager.getUserById(39)
+  t.not(res, null, 'la resuesta no debe ser null')
+  t.true(res.length > 0, 'la lista debe ser mayor a 0')
+})
